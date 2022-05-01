f8e6d9076e0de24343c67ba55b8c476c73dd5d0b
Sonja Wesa <sonja.wesa@outlook.com>
2022-05-01 18:16:39 +0300

    Harjoitus 3

---
+++ extras.js
@@ -0,0 +1,8 @@
+function someNakyvyys() {
+  var someNapit = document.getElementById("myLinks");
+  if (someNapit.style.display === "none") {
+    someNapit.style.display = "block";
+  } else {
+    someNapit.style.display = "none";
+  }
+}
